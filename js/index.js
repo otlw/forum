bfaa38a9af61f2c2405da6836f4330e698378046
Jared Pereira <jared@otlw.co>
2016-10-24 12:20:46 -0400

    add latest posts section

--- js/index.js
+++ js/index.js
@@ -18,4 +18,31 @@ function init() {
       window.location.href = 'page.html?query=' + document.getElementById('search-post').value;
     }
   }
+
+  forum = web3.eth.contract(forumABI).at(forumAddress);
+  var postMade = forum.PostMade({}, {fromBlock: 0, toBlock: 'latest'});
+  postMade.watch(function(error, result){
+    var postLink = document.createElement('a');
+    postLink.href = "page.html?query="+result.args._postAddress;
+    postLink.id = "post" + result.args._postAddress;
+    postLink.style = "text-decoration: none;";
+    var post = document.createElement("fieldset");
+    postTitle = document.createElement('span')
+    postTitle.id = post.id + "title";
+    forum.getTitle(result.args._postAddress, function(error, result) {
+      postTitle.textContent = result;
+    });
+    var numberOfReplies = document.createElement('span');
+    numberOfReplies.className = "subtitle";
+    numberOfReplies.href = "page.html?query="+result.args._postAddress;
+    numberOfReplies.style = "float: right;";
+    forum.getReplies(result.args._postAddress, function(err, res){
+      numberOfReplies.innerHTML = res.length.toString() + " reply[s]";
+    });
+
+    document.getElementById("latest").appendChild(postLink)
+    postLink.appendChild(post);
+    post.appendChild(postTitle);
+    post.appendChild(numberOfReplies);
+  })
 }
